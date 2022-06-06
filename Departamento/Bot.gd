067a071f02384077a1ce3a668451cc57b1a54318
rakso-dev <oskarr.magana@gmail.com>
2022-06-05 22:15:54 -0500

    small changes

--- Departamento/Bot.gd
+++ Departamento/Bot.gd
@@ -1,16 +1,25 @@
 extends KinematicBody2D
+var mov = Vector2() 
 
-
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-
-
-# Called when the node enters the scene tree for the first time.
 func _ready():
+	mov.x = 200
+	mov.y = 0
 	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+func _process(delta):
+	if $right.is_colliding():# and $up.is_colliding():
+		mov.x = 0
+		mov.y = 100
+	if $down.is_colliding() :#and $left.is_colliding():
+		mov.x = -100
+		mov.y = 0
+	if $left.is_colliding() :#and $down.is_colliding():
+		mov.x = 0
+		mov.y = -100
+	if $up.is_colliding() :#and $right.is_colliding():
+		mov.x = 100
+		mov.y = 0
+	move_and_collide(mov*delta)	
+	pass
